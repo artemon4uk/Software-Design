e0896fe4948d7477d4f76ce7cbe5cded71b1eb02
artemon4uk <37305117+artemon4uk@users.noreply.github.com>
2020-02-10 20:33:09 +0300

    Update circle.java

--- hw1/circle.java
+++ hw1/circle.java
@@ -1,14 +1,14 @@
 public class circle extends shape {
 	
-    private double radius;
+	private double radius;
 	
 	
-    public circle (double radius) {
-	this.radius = radius;
-    }
+	public circle (double radius) {
+		this.radius = radius;
+    	}
 	
-    public double getArea() {
-        double area = Math.PI * Math.pow(radius, 2);
-        return area;
-    }
+	public double getArea() {
+	        double area = Math.PI * Math.pow(radius, 2);
+        	return area;
+    	}
 }
